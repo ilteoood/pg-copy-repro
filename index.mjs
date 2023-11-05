c8bf261cc7d6ad1719cda7ebaa47b649239da65d
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-11-05 12:48:59 +0100

    fix: code

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import { randomUUID } from 'crypto'
 import knex from 'knex'
+import Papa from 'papaparse'
 import pgCopyStreams from 'pg-copy-streams'
 import { PassThrough, Readable, Transform, pipeline } from 'stream'
 import { pipeline as asyncPipeline } from 'stream/promises'
@@ -21,13 +22,14 @@ const knexInstance = knex({
 
 const init = async () => {
     await knexInstance.raw('create extension if not exists "uuid-ossp"')
+    await knexInstance.schema.dropTableIfExists('activities_consumptions')
     await knexInstance.schema.dropTableIfExists('activities')
+
     await knexInstance.schema.createTable('activities', (table) => {
         table.uuid('id').defaultTo(knexInstance.raw('uuid_generate_v4()')).primary()
         table.string('name')
     })
 
-    await knexInstance.schema.dropTableIfExists('activities_consumptions')
     await knexInstance.schema.createTable('activities_consumptions', (table) => {
         table.uuid('id').defaultTo(knexInstance.raw('uuid_generate_v4()')).primary()
         table.uuid('activity_id').references('id').inTable('activities')
@@ -43,6 +45,7 @@ const generateData = () => {
 
             const activity = {
                 id: activityId,
+                name: `Activity ${i}`,
                 consumptions: []
             }
 
@@ -55,27 +58,36 @@ const generateData = () => {
 
             yield activity
         }
-    })
+    }(), { objectMode: true })
 }
 
 const transformStream = (dataStream) => {
+    let isFirstActivity = true
+    let isFirstConsumption = true
+
     const activitiesStream = pipeline(
-        dataStream.pipe(new PassThrough()),
+        dataStream.pipe(new PassThrough({objectMode: true})),
         new Transform({
             objectMode: true,
-            transform: (data, _, callback) => {
-                callback(null, Papa.unparse([data]))
+            transform: ({consumptions, ...toInsert}, _, callback) => {
+                const csvData = Papa.unparse([toInsert], { header: false })
+
+                callback(null, isFirstActivity ? csvData : `\n${csvData}`)
+                isFirstActivity = false
             }
         }),
         console.log
     )
 
     const consumptionsStream = pipeline(
-        dataStream.pipe(new PassThrough()),
+        dataStream.pipe(new PassThrough({objectMode: true})),
         new Transform({
             objectMode: true,
             transform: (data, _, callback) => {
-                callback(null, Papa.unparse(data.consumptions))
+                const csvData = Papa.unparse(data.consumptions, { header: false })
+
+                callback(null, isFirstConsumption ? csvData : `\n${csvData}`)
+                isFirstConsumption = false
             }
         }),
         console.log
@@ -88,8 +100,8 @@ const insertData = async ({ activitiesStream, consumptionsStream }) => {
     const transaction = await knexInstance.transaction()
     const client = await transaction.client.acquireConnection()
 
-    const activitiesCopy = client.query(pgCopyStreams.copyTo('COPY activities (name) FROM STDIN WITH (FORMAT csv)'))
-    const consumptionsCopy = client.query(pgCopyStreams.copyTo('COPY activities_consumptions (activity_id, consumption) FROM STDIN WITH (FORMAT csv)'))
+    const activitiesCopy = client.query(pgCopyStreams.from('COPY activities (id, name) FROM STDIN WITH (FORMAT csv)'))
+    const consumptionsCopy = client.query(pgCopyStreams.from('COPY activities_consumptions (activity_id, consumption) FROM STDIN WITH (FORMAT csv)'))
 
     try {
         await Promise.all([
@@ -106,11 +118,16 @@ const insertData = async ({ activitiesStream, consumptionsStream }) => {
 }
 
 const main = async () => {
+    console.log('Creating tables')
     await init()
+
+    console.log('Generating data')
     const dataStream = generateData();
 
+    console.log('Transforming data')
     const transformedStream = transformStream(dataStream)
 
+    console.log('Inserting data')
     await insertData(transformedStream)
 
     await knexInstance.destroy()
